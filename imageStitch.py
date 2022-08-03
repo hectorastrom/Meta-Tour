f1d57b9758f85b811bd33811851285648d5f5632
William Y. Feng <womogenes2@gmail.com>
2022-08-02 20:07:25 -0400

    Reset everything to how it was

--- imageStitch.py
+++ imageStitch.py
@@ -4,7 +4,11 @@ import os
 import time
 import json
 import numpy as np
+from matplotlib import pyplot as plt
 import sys
+
+# Suppresses scientific notation
+np.set_printoptions(suppress=True)
 
 
 def stitch_images():
@@ -21,36 +25,36 @@ def stitch_images():
         # Reads images from folder and adds them to images list, resizing them
         for imgName in myList:
             curImg = cv.imread(f'{path}/{imgName}')
-            curImg = cv.resize(curImg, (0, 0), None, .5, .5)
+            curImg = cv.resize(curImg, (0,0), None, .5, .5)
             images.append(curImg)
             # cv.imshow(imgName, curImg)
 
         print("[INFO] Images Parsed")
 
         stitcher = cv.Stitcher_create()
-        (status, result) = stitcher.stitch(images)
+        (status,result) = stitcher.stitch(images)
         if (status == 0):
             print(f"[SUCCESS]: Image Sphere Generated for {room}")
             cv.imwrite(f"Stitches/stitch-{room}.jpg", result)
-            print(
-                f"[INFO]: Time elapsed to stitch {room} was {round(time.time()-start_time, 3)} seconds.")
-            cv.imshow(room, result)
+            print(f"[INFO]: Time elapsed to stitch {room} was {round(time.time()-start_time, 3)} seconds.")
+            cv.imshow(room,result)
             cv.waitKey(0)
         elif status == 1:
             print(f"[ERROR] Not enough keypoints in images of {room}")
-        else:
+        else: 
             print(f"[ERROR]: {room} Status {status}")
 
 
-def convert_milli_to_frames(milli: int, totalMilli: int, totalFrames: int):
+
+def convert_milli_to_frames(milli : int, totalMilli: int, totalFrames : int):
     """Returns the frame in a video given a specific timestamp in milliseconds"""
-    ratio = milli / totalMilli
-    return np.floor(ratio * totalFrames)
+    ratio = milli/totalMilli
+    return np.floor(ratio*totalFrames)
 
 
 def select_timestamps(rotations, distance: float):
     """Creates a list of all the desired timestamps for a given rotation distance between frames. Selects the frame closest to the next step. It will return a list of length: 360/distance full of timestamps in milliseconds."""
-    if distance <= 0:
+    if distance <= 0: 
         print("[ERROR]: Distance must be > 0")
         exit(1)
     # Timestamps stores our relevant timestamps
@@ -59,12 +63,12 @@ def select_timestamps(rotations, distance: float):
     startRotation = rotations[0][0]
     timestamps.append(rotations[0][1])
     # We want a point every distance degrees, so 360/distance points + 1 for the start
-    length = 360 // distance + 1
+    length = 360//distance+1
     for i in range(1, length):
         # Finding our desired rotation
-        desRot = startRotation + i * distance
+        desRot = startRotation + i*distance
         # Gets the index of where desired rotation would fit in our array, checks to make sure we don't go out of bounds
-        index = max(np.searchsorted(rotations[:, 0], desRot) - 1, 0)
+        index = max(np.searchsorted(rotations[:,0], desRot) -1, 0)
         timestamps.append(rotations[index][1])
     return timestamps
 
@@ -75,9 +79,9 @@ def load_video_frames(vidcap, frameList: list, resizeCoeff: float, flip: bool):
     # Read in image and success status
     for frame in frameList:
         vidcap.set(1, frame)
-        success, image = vidcap.read()
+        success,image = vidcap.read()
         if success:
-            image = cv.resize(image, (0, 0), None, resizeCoeff, resizeCoeff)
+            image = cv.resize(image, (0,0), None, resizeCoeff, resizeCoeff)
             if flip:
                 image = cv.rotate(image, cv.ROTATE_180)
             frames.append(image)
@@ -85,7 +89,6 @@ def load_video_frames(vidcap, frameList: list, resizeCoeff: float, flip: bool):
             # cv.waitKey(2)
 
     return frames
-
 
 def load_json(filename: str):
     """Returns a dictionary from a json file"""
@@ -99,21 +102,20 @@ def check_folder(folderName: str):
     currentPath = os.getcwd()
     folderPath = os.path.join(currentPath, folderName)
     if not os.path.isdir(folderPath):
-        print(
-            f"[INFO]: '{folderName}' not found, automatically created new folder")
+        print(f"[INFO]: '{folderName}' not found, automatically created new folder")
         os.mkdir(folderPath)
         exit(1)
         return False
     return True
 
 
-def videoToPanorama(tourData: str, videoName: str, scaleCoeff: int):
+def videoToPanorama(dataName : str, videoName : str, scaleCoeff: int):
     """Takes in the name of a json file with odometry data and the name of a video file and stitches a panorama stored in stitches/. Files whose names are inputted should exist in the most outside directory (same directory as imageStitch.py)"""
+    tourData = load_json(dataName)
     vidcap = cv.VideoCapture(videoName)
     totalFrames = vidcap.get(7)
-    # Creates a list of tuples with all timestamps and y absolute rotations (relevant rotation).
-    rotations = np.array([(row[9] * 180 / np.pi, row[0])
-                         for row in tourData[::]])
+    # Creates a list of tuples with all timestamps and y absolute rotations (relevant rotation). 
+    rotations = np.array([(row[9]*180/np.pi, row[0]) for row in tourData[::]])
     totalMilli = rotations[-1][1]
 
     # Sorts the array by orientation ascending (starts around 0, ends around 360)
@@ -128,54 +130,52 @@ def videoToPanorama(tourData: str, videoName: str, scaleCoeff: int):
     timestamps = np.array(select_timestamps(rotations, 12))
     frames = convert_milli_to_frames(timestamps, totalMilli, totalFrames)
     images = load_video_frames(vidcap, frames, scaleCoeff, True)
-    print(f"[INFO]: {len(images)} images gathered")
+    print("[INFO]: Images gathered")
     print("[INFO]: Stitching images...")
 
     start_time = time.time()
     stitcher = cv.Stitcher_create()
-    (status, result) = stitcher.stitch(images)
+    (status,result) = stitcher.stitch(images)
     if (status == 0):
         print(f"[SUCCESS]: Image Sphere Generated for {videoName}")
         cv.imwrite(f"Stitches/{videoName[:-5]}_stitch.jpg", result)
-        print(
-            f"[INFO]: Time elapsed to stitch {videoName} was {round(time.time()-start_time, 3)} seconds.")
-        cv.imshow(videoName, result)
+        print(f"[INFO]: Time elapsed to stitch {videoName} was {round(time.time()-start_time, 3)} seconds.")
+        cv.imshow(videoName,result)
         cv.waitKey(0)
         return result
     elif status == 1:
         print(f"[ERROR] Not enough keypoints in images of {videoName}")
         return 1
-    else:
+    else: 
         print(f"[ERROR]: {videoName} Status {status}")
         return 1
+    
 
+# check for valid num of arguments
+if len(sys.argv) != 4:
+    print("[ERROR]: usage: python imageStitch.py 'datafile.json' 'videofile.webm' float: scaleCoefficient")
+    exit(1)
 
-if __name__ == "__main__":
-    # check for valid num of arguments
-    if len(sys.argv) != 4:
-        print("[ERROR]: usage: python imageStitch.py 'datafile.json' 'videofile.webm' float: scaleCoefficient")
-        exit(1)
+jsonFile = sys.argv[1]
+videoFile = sys.argv[2]
+# Test if scalecoefficient is inputted as an integer
+try:
+    scaleCoeff = float(sys.argv[3])
+except ValueError: 
+    print("[ERROR] scale coefficient not float")
+    exit(1)
 
-    jsonFile = sys.argv[1]
-    videoFile = sys.argv[2]
-    # Test if scalecoefficient is inputted as an integer
-    try:
-        scaleCoeff = float(sys.argv[3])
-    except ValueError:
-        print("[ERROR] scale coefficient not float")
-        exit(1)
+if not jsonFile.endswith(".json"): 
+    print("First argument must be name of JSON file")
+    exit(1)
 
-    if not jsonFile.endswith(".json"):
-        print("First argument must be name of JSON file")
-        exit(1)
+if not videoFile.endswith(".webm"): 
+    # Technically we can accept a bunch of video file types I think, but since this
+    # is what we've been getting from the website this is what we have for now
+    print("First argument must be name of a webm file")
+    exit(1)
 
-    if not videoFile.endswith(".webm"):
-        # Technically we can accept a bunch of video file types I think, but since this
-        # is what we've been getting from the website this is what we have for now
-        print("First argument must be name of a webm file")
-        exit(1)
+if (check_folder("Data") and check_folder("Stitches")):
+    print("[INFO]: All necessary folders exist")
 
-    if (check_folder("Data") and check_folder("Stitches")):
-        print("[INFO]: All necessary folders exist")
-
-    videoToPanorama(load_json(jsonFile), videoFile, scaleCoeff=1)
+videoToPanorama(jsonFile, videoFile, scaleCoeff=1)
